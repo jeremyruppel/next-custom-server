18544b825670440e088d7c743ee6c8ed1b6195c1
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2023-07-14 13:09:50 -0400

    working in 13.4.2

---
+++ server.js
@@ -0,0 +1,40 @@
+// @ts-check
+const express = require('express')
+/** @type {import('next').default}*/
+// @ts-ignore
+const next = require('next')
+const { parse } = require('url')
+
+const dev = true
+
+const app = express()
+
+const nextApp = next({ dev })
+
+const nextHandler = nextApp.getRequestHandler()
+
+/** @type {import('express').RequestHandler} */
+const handler = async (req, res, next) => {
+  try {
+    const parsedUrl = parse(req.originalUrl, true)
+    await nextHandler(req, res, parsedUrl)
+  } catch (err) {
+    // this never happens btw
+    next(err)
+  }
+}
+
+nextApp.prepare().then(() => {
+  app.get('/foo', handler)
+
+  if (dev) {
+    app.use('/_next/', handler)
+  }
+
+  app.use((req, res) => res.sendStatus(404))
+
+
+  app.listen(3000, () => {
+    console.log('ready on http://localhost:3000')
+  })
+})
